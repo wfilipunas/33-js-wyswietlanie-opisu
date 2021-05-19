275f3b5de5f76ad84510db5bfb2441b4349de490
wfilipunas <wfilipunas@gmail.com>
2021-05-19 08:00:53 +0200

    onclick

---
+++ js/main.js
@@ -0,0 +1,7 @@
+let myParagraph = document.getElementById('opis');
+let myButton = document.querySelector('button');
+
+myButton.onclick = function(){
+    
+    myParagraph.textContent = 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.'
+}
